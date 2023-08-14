4aff32434ebf6a0a55a18e26d2d78d223dcba9fc
rhyslopez-js <rhys.paulino@vanarts.com>
2023-08-13 22:56:33 -0700

    added gsap animations on home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,34 @@ import gsap from 'gsap';
 export class HomeComponent {
 
 
+  
+
   ngAfterViewInit(): void{
     gsap.from('.pageTitle', {duration: 1, x: -100, ease: "bounce"})
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Select the element you want to fade in
+    const fadeElement = document.querySelector('.fade-in');
+
+    // Create a GSAP timeline for the animation
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: fadeElement,
+            start: "top center", // The element triggers when its center reaches the top of the viewport
+        }
+    });
+
+
+    tl.from(fadeElement, {
+      opacity: 0,
+  });
+
+    // Add the fade-in animation to the timeline
+    tl.to(fadeElement, {
+        opacity: 1,
+        ease: "bounce"
+    });
   }
 
 
